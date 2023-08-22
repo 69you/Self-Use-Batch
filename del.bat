311cf97adf5f6b9bab43ae070366410c6afe5fb1
侑介 <spark20060609@gmail.com>
2023-08-22 19:19:13 +0800

    .

--- del.bat
+++ del.bat
@@ -1,4 +1,5 @@
 @REM delete files that are undeletable
+
 DEL /F /A /Q \\?\%1
 
 RD /S /Q \\?\%1
--- usb.bat
+++ usb.bat
@@ -1,2 +1,3 @@
 @REM open USB driver
+
 %windir%\system32\rundll32.exe shell32.dll,Control_RunDLL hotplug.dll
